bc0dd67ee10d42e09e6e1d59af0b5e58070d77e6
tntntnth7 <tntntnth@naver.com>
2020-03-15 23:48:19 +0900

    LOGGING, CONTEXT-PATH 처리

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -6,17 +6,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @RestController
 public class UserController {
 	
+	private static final Logger LOGGER = LogManager.getLogger(UserController.class);
+	
 	@Autowired
 	UserService userService;
 	
-	@RequestMapping(value = "/user",method = RequestMethod.GET)
+	@RequestMapping(value = "/",method = RequestMethod.GET)
 	public String selectListUser() {
-		System.out.println("## UserController");
 		
+		// log 사용법
+		LOGGER.info("## UserController ##");
+		LOGGER.debug("## DEBUG ##");
+        LOGGER.error("## Error ##");
+        
 		userService.selectListUser();
 		
 		return "user";
